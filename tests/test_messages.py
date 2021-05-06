a52ad2007fcf9c23311d54cfe56b47813ce08377
Oliver Rice <github@oliverrice.com>
2021-05-05 22:25:22 -0500

    install p-hooks

--- tests/test_messages.py
+++ tests/test_messages.py
@@ -1,11 +1,14 @@
 import pytest
-from realtime.message import parse, Message, TransactionMessage, CRUDMessage, Column
+
+from realtime.message import Column, CRUDMessage, TransactionMessage, parse
 
 
-@pytest.mark.parametrize("message,result", [
-    ("BEGIN 501", TransactionMessage(command="BEGIN", lsn=501)),
-    ("COMMIT 95", TransactionMessage(command="COMMIT", lsn=95))
-    ]
+@pytest.mark.parametrize(
+    "message,result",
+    [
+        ("BEGIN 501", TransactionMessage(command="BEGIN", lsn=501)),
+        ("COMMIT 95", TransactionMessage(command="COMMIT", lsn=95)),
+    ],
 )
 def test_parse_transaction(message: str, result: TransactionMessage) -> None:
     assert parse(message) == result
@@ -14,12 +17,12 @@ def test_parse_transaction(message: str, result: TransactionMessage) -> None:
 def test_parse_crud() -> None:
     message = "table public.account: INSERT: id[integer]:5 email[text]:'e@e.c' is_e_vd[boolean]:false"
     assert parse(message) == CRUDMessage(
-            command="INSERT",
-            schema="public",
-            table="account",
-            columns=[
-                Column(column="id", data_type="integer", value="5"),
-                Column(column="email", data_type="text", value="'e@e.c'"),
-                Column(column="is_e_vd", data_type="boolean", value="false"),
-            ]
+        command="INSERT",
+        schema="public",
+        table="account",
+        columns=[
+            Column(column="id", data_type="integer", value="5"),
+            Column(column="email", data_type="text", value="'e@e.c'"),
+            Column(column="is_e_vd", data_type="boolean", value="false"),
+        ],
     )
